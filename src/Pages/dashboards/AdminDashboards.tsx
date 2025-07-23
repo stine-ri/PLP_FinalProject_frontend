20c85b310b7ee177e684eecfdc07f2834d1e5d04
Christine Nyambwari <christinenyambwari@gmail.com>
2025-07-23 05:03:10 -0700

    commit

--- src/Pages/dashboards/AdminDashboards.tsx
+++ src/Pages/dashboards/AdminDashboards.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useDispatch } from "react-redux";
 import { logout } from "../../redux/slices/authSlice";
 import { useNavigate } from "react-router-dom";
--- src/Pages/teachers/Homework.tsx
+++ src/Pages/teachers/Homework.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { HomeworkList } from '../../Components/HomeworkList';
 
 export const TeacherDashboard = () => {
