2614fa7ce640f865e890448426352468a36556b7
Christine Nyambwari <christinenyambwari@gmail.com>
2025-07-23 05:14:21 -0700

    commit

--- src/Pages/about.tsx
+++ src/Pages/about.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import  { useRef, useEffect } from "react";
 import { motion, useAnimation, useInView } from "framer-motion";
 import Navbar from '../Components/Navbar';
 
